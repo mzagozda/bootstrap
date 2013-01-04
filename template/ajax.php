69b4bbd855563a6a55430739f4fd78b76d3ef255
mzagozda <maciej.zagozda@gmail.com>
2013-01-04 03:53:55 +0100

    Improved AJAX validation

--- template/ajax.php
+++ template/ajax.php
@@ -1,3 +1,11 @@
 <?
 require_once DOC_ROOT . '/config/config.php';
-header('Content-type: application/json');
+
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&  $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')
+{
+    header('Content-type: application/json');
+}
+else
+{
+    die('This page cannot be accessed directly');
+}
